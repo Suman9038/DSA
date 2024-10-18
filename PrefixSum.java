b98106f2fa6ceedf377e20fd2a1f41f5afa08c79
Suman9038 <149608702+Suman9038@users.noreply.github.com>
2024-10-18 21:52:54 +0000

    Another variation of Prefix Sum

--- PrefixSum.java
+++ PrefixSum.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class PrefixSum {
     /*
      * 1. Given a fixed array and multiple quireies of following types on array, how
@@ -78,9 +79,43 @@ public class PrefixSum {
         }
         return false;
     }
+    /*
+     * Given n ranges find the maximum appearance element in the ranges
+     */
+    // NAIVE SOLN IS USING HASHING
+
+    // EFFECIENT SOLN
+
+    static int maxAppear(int l[],int r[])
+    {
+        int l1=l.length;
+        int l2=r.length;
+        int n;
+        int arr [] =new int[1000];
+        for(int i=0;i<l1;i++)
+        {
+            arr[l[i]]++;
+            arr[r[i]+1]--;
+
+        }
+        int max=arr[0];
+        int res=0;
+        for(int i =1;i<1000;i++)
+        {
+            arr[i]+=arr[i-1];
+            if(max<arr[i])
+            {
+                max=arr[i];
+                res=i;
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         // int arr[]={2,8,3,9,6,5,4};
         int arr[]={3,4,8,-9,20,6};
+        int l[]={1,2,5,15};
+        int r[]={5,8,7,18};
         
         int n=arr.length;
         pre_sum = new int[n];
@@ -90,8 +125,9 @@ public class PrefixSum {
             pre_sum[i]=pre_sum[i-1]+arr[i];
         }
         // getSum(arr, 0, 2);
-       System.out.println(eqSum(arr)); 
-       System.out.println(equiliSum(arr));
+    //    System.out.println(eqSum(arr)); 
+    //    System.out.println(equiliSum(arr));
+       System.out.println(maxAppear(l, r));
         // System.out.println(Sum(arr, 0, 2));
         // System.out.println(Sum(arr, 1, 3));
         // System.out.println(Sum(arr, 2, 6));
